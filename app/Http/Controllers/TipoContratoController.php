b097cc69ceb871de3ca6cdc6419419598029a224
Alith <alithferr95@gmail.com>
2024-09-23 21:28:25 -0500

    Se crearon metodos y rutas de los modelos del proyecto

--- app/Http/Controllers/TipoContratoController.php
+++ app/Http/Controllers/TipoContratoController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class TipoContratoController extends Controller
 {
-    //
+ //
+ public function save(Request $REQUEST){
+    return response()->json([
+        'status' => '200',
+        'message' => 'guardado con exito',
+    ]);
+}
+public function getData (Request $REQUEST){
+    return response()->json([
+        'status' => '200',
+        'message' => 'data..',
+    ]);
+}
+public function update (Request $REQUEST){
+    return response()->json([
+        'status' => '200',
+        'message' => 'guardado con exito',
+    ]);
+}
+public function delete (Request $REQUEST){
+    return response()->json([
+        'status' => '200',
+        'message' => 'Se elimino con exito',
+    ]);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AsistenciaController;
+use App\Http\Controllers\BeneficioController;
+use App\Http\Controllers\ContratosLaboraleController;
+use App\Http\Controllers\EmpleadoController;
+use App\Http\Controllers\PrestacionesSocialeController;
+use App\Http\Controllers\TipoBeneficioController;
+use App\Http\Controllers\TipoContratoController;
+use App\Http\Controllers\TipoPrestacionesSocialeController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +24,43 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/Asistencia/getData', [AsistenciaController::class, 'getData']); 
+Route::put('/Asistencia/update', [AsistenciaController::class, 'update']); 
+Route::delete('/Asistencia/delete', [AsistenciaController::class, 'delete']); 
+Route::post('/Asistencia/save', [AsistenciaController::class, 'save']); 
+
+Route::get('/Beneficio/getData', [BeneficioController::class, 'getData']); 
+Route::put('/Beneficio/update', [BeneficioController::class, 'update']); 
+Route::delete('/Beneficio/delete', [BeneficioController::class, 'delete']); 
+Route::post('/Beneficio/save', [BeneficioController::class, 'save']); 
+
+Route::get('/ContratosLaborale/getData', [ContratosLaboraleController::class, 'getData']); 
+Route::put('/ContratosLaborale/update', [ContratosLaboraleController::class, 'update']); 
+Route::delete('/ContratosLaborale/delete', [ContratosLaboraleController::class, 'delete']); 
+Route::post('/ContratosLaborale/save', [ContratosLaboraleController::class, 'save']); 
+
+Route::get('/empleado/getData', [EmpleadoController::class, 'getData']); 
+Route::put('/empleado/update', [EmpleadoController::class, 'update']); 
+Route::delete('/empleado/delete', [EmpleadoController::class, 'delete']); 
+Route::post('/empleado/save', [EmpleadoController::class, 'save']); 
+
+Route::get('/PrestacionesSociale/getData', [PrestacionesSocialeController::class, 'getData']); 
+Route::put('/PrestacionesSociale/update', [PrestacionesSocialeController::class, 'update']); 
+Route::delete('/PrestacionesSociale/delete', [PrestacionesSocialeController::class, 'delete']); 
+Route::post('/PrestacionesSociale/save', [PrestacionesSocialeController::class, 'save']); 
+
+Route::get('/TipoBeneficio/getData', [TipoBeneficioController::class, 'getData']); 
+Route::put('/TipoBeneficio/update', [TipoBeneficioController::class, 'update']); 
+Route::delete('/TipoBeneficio/delete', [TipoBeneficioController::class, 'delete']); 
+Route::post('/TipoBeneficio/save', [TipoBeneficioController::class, 'save']); 
+
+Route::get('/TipoContrato/getData', [TipoContratoController::class, 'getData']); 
+Route::put('/TipoContrato/update', [TipoContratoController::class, 'update']); 
+Route::delete('/TipoContrato/delete', [TipoContratoController::class, 'delete']); 
+Route::post('/TipoContrato/save', [TipoContratoController::class, 'save']); 
+
+Route::get('/TipoPrestacionesSociale/getData', [TipoPrestacionesSocialeController::class, 'getData']); 
+Route::put('/TipoPrestacionesSociale/update', [TipoPrestacionesSocialeController::class, 'update']); 
+Route::delete('/TipoPrestacionesSociale/delete', [TipoPrestacionesSocialeController::class, 'delete']); 
+Route::post('/TipoPrestacionesSociale/save', [TipoPrestacionesSocialeController::class, 'save']); 
